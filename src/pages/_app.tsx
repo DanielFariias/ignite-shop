a27d10689d88d8021d3acba2a5c7dccadd5d4aaa
DanielFariias <danielfariias15@gmail.com>
2023-08-26 12:53:57 -0300

    feat: :art: Header created

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,19 @@ import type { AppProps } from 'next/app'
 
 import { globalStyles } from '@styles/global'
 
+import logoSvg from '@assets/logo.svg'
+import * as S from '@styles/pages/app'
+
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <S.Container>
+      <S.Header>
+        <img src={logoSvg.src} alt="" />
+      </S.Header>
+
+      <Component {...pageProps} />
+    </S.Container>
+  )
 }
